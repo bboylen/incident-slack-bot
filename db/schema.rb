664d250e8b3f30922291dd8d319a699d7bd0d815
BB <brian.boylen@sofwarellc.com>
2023-04-15 20:19:36 -0400

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_04_12_014037) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "incidents", force: :cascade do |t|
     t.string "title", null: false
     t.string "description"
